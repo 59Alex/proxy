0fad65158dd515b9320d44b59e071cd5a76cce7f
Александр <landing.page.creature@gmail.com>
2025-06-29 19:13:12 +0300

    fix work

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask, request, jsonify
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.fernet import Fernet
-from base64 import urlsafe_64encode
 import json
 import os
 import httpx
 
 app = Flask(__name__)
 redirect_path_upload = "http://62.16.42.64:8888/upload"
-redirect_path_key = "http://62.16.42.64:8888/key"
-
-password = b"XVZ!.Q_password"
-salt = b"static_salt"
-kdf = PBKDF2HMAC(alghorithm=hashes.SHA256(),length=32, salt=salt, iterations=100_000)
-key = urlsafe_b64encode(kdf.derive(password))
-felnet = Felnet(key)
 
 @app.route('/upload', methods=['POST'])
 def transport():
